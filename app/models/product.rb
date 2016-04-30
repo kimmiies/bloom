70efe92fa16e45de0118152795d2d51751e5f96a
Kimberly Oleiro <koleiro@gmail.com>
2016-04-29 20:32:33 -0400

    Displaying validation errors on the product form

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ActiveRecord::Base
-  validates :description, :name, :url, :size, presence: true
+  validates :description, :name, :url, :size, :price_in_cents, presence: true
   validates :price_in_cents, numericality: {only_integer: true}
 end
