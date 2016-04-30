c87143e5eba48b473486616b7c125bd33427c813
Kimberly Oleiro <koleiro@gmail.com>
2016-04-30 08:46:42 -0400

    Created the form for a new user, added error messages. Also added validations to User model for presence of all fields and uniqueness of email and username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :username, :name, :email, presence: true
+  
+  validates :username, :email, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   resources :products
 
-  resources :users, only: [:new, :create]
+  resources :users, only: [:show, :new, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -60,7 +60,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
-ucts
+
   #   end
 end
